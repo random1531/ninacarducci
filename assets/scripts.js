dc99f37d69a9219354f9b8a08ad687cd4806cf4b
random1531 <loic.raval@gmail.com>
2024-09-04 20:00:40 +0200

    modified:   .idea/.gitignore 	deleted:    assets/bootstrap/bootstrap.bundle.js 	deleted:    assets/bootstrap/bootstrap.bundle.js.map 	modified:   assets/bootstrap/bootstrap.bundle.min.js 	deleted:    assets/bootstrap/bootstrap.bundle.min.js.map 	deleted:    assets/bootstrap/bootstrap.css 	deleted:    assets/bootstrap/bootstrap.css.map 	modified:   assets/bootstrap/bootstrap.min.css 	deleted:    assets/bootstrap/bootstrap.min.css.map     modified:   assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.webp 	modified:   assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.webp 	modified:   assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.webp 	modified:   assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.webp 	modified:   assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.webp 	modified:   assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.webp 	modified:   assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-min.webp 	modified:   assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.webp 	modified:   assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.webp 	modified:   assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.webp 	modified:   assets/index.js 	modified:   assets/scripts.js 	modified:   assets/style.css 	modified:   index.html

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.gallery').mauGallery({
         columns: {
             xs: 1,
@@ -13,3 +13,6 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+
+
+
